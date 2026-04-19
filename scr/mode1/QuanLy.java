0fc6cd6230e286a6b8c32382fad37ad42b0dc6d4
Phung Phi <phungphi4989@gmail.com>
2026-04-19 18:26:32 +0700

    Khoi tao 3 class model SanPham, KhachHang, QuanLy cho do an

---
+++ scr/mode1/QuanLy.java
@@ -0,0 +1,39 @@
+package scr.mode1;
+
+public class QuanLy {
+    private String maQL;
+    private String tenQL;
+    private String taiKhoan;
+    private String matKhau;
+    private String vaiTro;
+
+    
+    public QuanLy() {
+    }
+
+    
+    public QuanLy(String maQL, String tenQL, String taiKhoan, String matKhau, String vaiTro) {
+        this.maQL = maQL;
+        this.tenQL = tenQL;
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+        this.vaiTro = vaiTro;
+    }
+
+    
+    public String getMaQL() { return maQL; }
+    public void setMaQL(String maQL) { this.maQL = maQL; }
+
+    public String getTenQL() { return tenQL; }
+    public void setTenQL(String tenQL) { this.tenQL = tenQL; }
+
+    public String getTaiKhoan() { return taiKhoan; }
+    public void setTaiKhoan(String taiKhoan) { this.taiKhoan = taiKhoan; }
+
+    public String getMatKhau() { return matKhau; }
+    public void setMatKhau(String matKhau) { this.matKhau = matKhau; }
+
+    public String getVaiTro() { return vaiTro; }
+    public void setVaiTro(String vaiTro) { this.vaiTro = vaiTro; }
+}
+
---
+++ scr/mode1/SanPham.java
@@ -0,0 +1,85 @@
+package scr.mode1;
+
+
+   public class SanPham {
+    private String maSP;
+    private String tenSP;
+    private String loai;
+    private String size;
+    private String mau;
+    private double gia;
+    private int soLuong;
+
+    
+    public SanPham() {
+    }
+
+    
+    public SanPham(String maSP, String tenSP, String loai, String size, String mau, double gia, int soLuong) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.loai = loai;
+        this.size = size;
+        this.mau = mau;
+        this.gia = gia;
+        this.soLuong = soLuong;
+    }
+
+    
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public String getLoai() {
+        return loai;
+    }
+
+    public void setLoai(String loai) {
+        this.loai = loai;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getMau() {
+        return mau;
+    }
+
+    public void setMau(String mau) {
+        this.mau = mau;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+}
+
